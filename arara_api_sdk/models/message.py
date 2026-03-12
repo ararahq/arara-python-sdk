421488d7c460f41c3c5cc53e2be948fc8389ed32
Micael Marques <micasmarques1132@gmail.com>
2026-03-12 13:35:15 -0300

    fix: correcao docs

--- arara_api_sdk/models/message.py
+++ arara_api_sdk/models/message.py
@@ -12,7 +12,7 @@ class SendMessageRequest(BaseModel):
     smart_link_param: Optional[str] = Field(None, alias="smartLinkParam")
     smart_link_url: Optional[str] = Field(None, alias="smartLinkUrl")
     body: Optional[str] = None
-    media_url: Optional[str] = Field(None, alias="media_url")
+    media_url: Optional[str] = Field(None, alias="media_url")  # Deprecated: use variables[0] for header media
     scheduled_at: Optional[datetime] = Field(None, alias="scheduled_at")
     mode: Optional[str] = None  # LIVE or TEST
 
